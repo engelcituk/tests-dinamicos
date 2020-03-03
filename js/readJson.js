d3e7aed8bf7fc45d667ff0a36bdf1a38051e2e0d
cituk <programaciondyn3.mx@sandos.com>
2020-03-02 21:40:48 -0500

    lectura dinamica de archivo json de monedas de conversion y pintado de tabla de manera dinamica

---
+++ js/readJson.js
@@ -0,0 +1,44 @@
+
+generarListado();
+
+function generarListado(){
+    readJson().then( monedas => {
+        generarTabla(monedas.EURO);
+        generarTabla(monedas.USD);
+        generarTabla(monedas.MX);
+
+    }).catch( err=> {
+        console.log(err);
+    })
+}
+
+function readJson() {
+    const url = '../json/listado.json';
+   
+    return fetch(url)
+        .then( async response => {
+            return await response.json();
+        })
+        .then(result => {
+            return result;
+        })
+        .catch(err => {
+            return err;
+        })
+}
+
+ function generarTabla(monedas){
+    const listCoinsArray = [];
+    monedas.forEach(moneda => {
+        listCoinsArray.push({moneda});
+        lstTr=`<tr>
+                    <th scope="row">${moneda.de}</th>
+                    <th >${moneda.a}</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>`;
+        $("table tbody").append(lstTr);
+    });
+ }
